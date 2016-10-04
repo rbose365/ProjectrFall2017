c2bce416201f8f49f874a9d961bba7a8f864c1ae
rguthrie3 <rguthrie3@gatech.edu>
2016-10-04 18:46:12 -0400

    Added pseudo student login

--- project_management_system/views.py
+++ project_management_system/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from forms import LoginForm, RegisterForm
+
+test_student = {
+    "email": "student@du.com",
+    "password": "password",
+    "name": "Jeremy Corn",
+    "projects" : [
+        {
+            "project_name": "Space Race",
+            "project_description": "Lorem ipsum dolor sit amet, qualisque repudiare ut his, verear phaedrum disputando cu nec. \
+                    Nonumy iisque est ne, cu quo graeco dissentias. Id adipisci accommodare sea. Debet suscipit in ius, ad laboramus\
+                    gloriatur voluptaria qui. Sit alii habeo no, qui elitr constituto id."
+        },
+        {
+            "project_name": "Vim Package Manager",
+            "project_description": "Lorem ipsum dolor sit amet, qualisque repudiare ut his, verear phaedrum disputando cu nec. Nonumy\
+                    iisque est ne, cu quo graeco dissentias. Id adipisci accommodare sea. Debet suscipit in ius, ad laboramus gloriatur\
+                    voluptaria qui. Sit alii habeo no, qui elitr constituto id."
+        }],
+    "inbox" : [
+        {
+            "subject": "Message 1",
+            "body" : "Ad tation reprehendunt sit, ne eum autem dolor consectetuer. Ipsum diceret delenit vis ea, elitr maiestatis te ius.\
+                    Menandri electram interesset in quo, odio ocurreret cu mea. At nec consul corpora, in illum debet vocent sea, in nec\
+                    vivendo deterruisset."
+        },
+        {
+            "subject": "Message 2",
+            "body": "Veritus nominavi appareat at mel, vel commune scriptorem ea. Suscipit oportere ne vel. Ea appareat constituam definitiones\
+                    mel. Nobis utinam prodesset ne usu, vis ut utroque facilis voluptua, ut tale assueverit nec. Sit novum mundi moderatius ut,\
+                    cu prodesset definitionem has, mel te sonet principes."
+        }],
+    "notifications" : [
+        {
+            "subject": "Notification 1",
+            "body" : "Ad tation reprehendunt sit, ne eum autem dolor consectetuer. Ipsum diceret delenit vis ea, elitr maiestatis te ius. Menandri\
+                    electram interesset in quo, odio ocurreret cu mea. At nec consul corpora, in illum debet vocent sea, in nec vivendo deterruisset."
+        },
+        {
+            "subject": "Notification 2",
+            "body": "Veritus nominavi appareat at mel, vel commune scriptorem ea. Suscipit oportere ne vel. Ea appareat constituam definitiones mel.\
+                     Nobis utinam prodesset ne usu, vis ut utroque facilis voluptua, ut tale assueverit nec. Sit novum mundi moderatius ut, cu prodesset\
+                     definitionem has, mel te sonet principes."
+        }]
+}
+
 
 # Create your views here.
 def index(request):
@@ -10,6 +56,16 @@ def login(request):
         form = LoginForm(request.POST)
         form.is_valid()
         print "Login Sumbitted:", form.cleaned_data
+        email = form.cleaned_data["email"]
+        password = form.cleaned_data["password"]
+        
+        # DEMO check for hard-coded user for demo
+        print email, password
+        print test_student["email"] == email
+        print test_student["password"] == password
+        if email == test_student["email"] and password == test_student["password"]:
+            return HttpResponseRedirect("/student/")
+        # ENDDEMO
     else:
         form = LoginForm()
     return render(request, "login.html", { "form": form })
@@ -88,51 +144,7 @@ def client(request):
     return render(request, "client.html", context)
 
 def student(request):
-    context = {
-            "student_name": "Jeremy Corn",
-            "projects" : [
-                {
-                    "project_name": "Space Race",
-                    "project_description": "Lorem ipsum dolor sit amet, qualisque repudiare ut his, verear phaedrum disputando cu nec. \
-                            Nonumy iisque est ne, cu quo graeco dissentias. Id adipisci accommodare sea. Debet suscipit in ius, ad laboramus\
-                            gloriatur voluptaria qui. Sit alii habeo no, qui elitr constituto id."
-                },
-                {
-                    "project_name": "Vim Package Manager",
-                    "project_description": "Lorem ipsum dolor sit amet, qualisque repudiare ut his, verear phaedrum disputando cu nec. Nonumy\
-                            iisque est ne, cu quo graeco dissentias. Id adipisci accommodare sea. Debet suscipit in ius, ad laboramus gloriatur\
-                            voluptaria qui. Sit alii habeo no, qui elitr constituto id."
-                }
-                ],
-            "inbox" : [
-                {
-                    "subject": "Message 1",
-                    "body" : "Ad tation reprehendunt sit, ne eum autem dolor consectetuer. Ipsum diceret delenit vis ea, elitr maiestatis te ius.\
-                            Menandri electram interesset in quo, odio ocurreret cu mea. At nec consul corpora, in illum debet vocent sea, in nec\
-                            vivendo deterruisset."
-                },
-                {
-                    "subject": "Message 2",
-                    "body": "Veritus nominavi appareat at mel, vel commune scriptorem ea. Suscipit oportere ne vel. Ea appareat constituam definitiones\
-                            mel. Nobis utinam prodesset ne usu, vis ut utroque facilis voluptua, ut tale assueverit nec. Sit novum mundi moderatius ut,\
-                            cu prodesset definitionem has, mel te sonet principes."
-                }
-                ],
-            "notifications" : [
-                {
-                    "subject": "Notification 1",
-                    "body" : "Ad tation reprehendunt sit, ne eum autem dolor consectetuer. Ipsum diceret delenit vis ea, elitr maiestatis te ius. Menandri\
-                            electram interesset in quo, odio ocurreret cu mea. At nec consul corpora, in illum debet vocent sea, in nec vivendo deterruisset."
-                },
-                {
-                    "subject": "Notification 2",
-                    "body": "Veritus nominavi appareat at mel, vel commune scriptorem ea. Suscipit oportere ne vel. Ea appareat constituam definitiones mel.\
-                             Nobis utinam prodesset ne usu, vis ut utroque facilis voluptua, ut tale assueverit nec. Sit novum mundi moderatius ut, cu prodesset\
-                             definitionem has, mel te sonet principes."
-                }
-                ]
-            }
-    return render(request, "student.html", context)
+    return render(request, "student.html", test_student)
 
 def projects(request):
     context = {
