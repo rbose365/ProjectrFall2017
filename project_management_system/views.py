857d643c8ae8988498cd05f70d7e81205f7062b0
rguthrie3 <rguthrie3@gatech.edu>
2016-10-05 14:31:42 -0400

    Wired together for demo

--- project_management_system/views.py
+++ project_management_system/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponseRedirect
 from forms import LoginForm, RegisterForm
 from samples import *
 
+user_type_logged_in = None
+
 # Create your views here.
 def index(request):
+    global user_type_logged_in
+    user_type_logged_in = None
     return render(request, "index.html")
 
 def login(request):
@@ -18,11 +22,15 @@ def login(request):
         # DEMO check for hard-coded user for demo
         print email, password
         print test_student["email"] == email
+        global user_type_logged_in
         if email == test_student["email"]:
+            user_type_logged_in = "student"
             return HttpResponseRedirect("/student/")
         elif email == test_instructor["email"]:
+            user_type_logged_in = "instructor"
             return HttpResponseRedirect("/instructor/")
         elif email == test_client["email"]:
+            user_type_logged_in = "client"
             return HttpResponseRedirect("/client/")
         # ENDDEMO
     else:
@@ -52,3 +60,19 @@ def projects(request):
 
 def project(request):
     return render(request, "project.html", test_project)
+
+def messages(request):
+    return render(request, "messages.html", test_messages)
+
+def menu(request):
+    print user_type_logged_in
+    if user_type_logged_in is None:
+        return render(request, "index.html")
+    elif user_type_logged_in == "student":
+        return render(request, "student.html", test_student)
+    elif user_type_logged_in == "instructor":
+        return render(request, "instructor.html", test_instructor)
+    elif user_type_logged_in == "client":
+        return render(request, "client.html", test_client)
+    else:
+        assert False, "Should never reach here"
