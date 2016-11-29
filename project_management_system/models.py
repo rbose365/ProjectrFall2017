a8a8981e44a482001703202bb8ad902bc3d6aa7b
rguthrie3 <rguthrie3@gatech.edu>
2016-11-28 21:21:05 -0500

    Added group invitation object and modified group

--- project_management_system/models.py
+++ project_management_system/models.py
@@ -6,6 +6,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class Group(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+
+
+class GroupInvitation(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    invitee = models.ForeignKey(User, related_name="invitee")
+    inviter = models.ForeignKey(User, related_name="inviter")
+
+
 class Profile(models.Model):
     USER_TYPES = (
         ('S', 'Student'),
@@ -14,6 +24,7 @@ class Profile(models.Model):
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(max_length=1, choices=USER_TYPES)
+    group = models.ForeignKey(Group, null=True)
 
 # These methods are for linking the Profile model with Django built-in User model for authentication
 # Reference: https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
@@ -34,32 +45,17 @@ class Project(models.Model):
     description = models.CharField(max_length=255)
     client = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class Question(models.Model):
     text = models.CharField(max_length=255)
     project = models.ForeignKey(Project, on_delete=models.CASCADE) # About (Question <-> Project) ; many to one
 
-#class Student(models.Model):
-#    email = models.CharField(max_length=255, primary_key=True)
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Has a (Student <-> Question) ; one to many
-
-# class Client(models.Model):
-#     email = models.CharField(max_length=255, primary_key=True)
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE) # Contracted By (Client <-> Project) ; one to many
-
-# class Group(models.Model):
-#     name = models.CharField(max_length=255, primary_key=True)
-#     student = models.ForeignKey(Student, on_delete=models.CASCADE)   # Belongs to (Student <-> Group) ; many to one
-#     bid = models.ForeignKey(Bid, on_delete=models.CASCADE)   # Has (Group <-> Bid) ; one to many
-
-# class Instructor(models.Model):
-#     email = models.CharField(max_length=255, primary_key=True)
-#     sections = models.ManyToManyField(Section) # Teaches (Instructor <-> Section) ; many to many
 
 class Section(models.Model):
     name = models.CharField(max_length=255)
     instructors = models.ManyToManyField(User, related_name="instructors_for_section")
     students = models.ManyToManyField(User, related_name="students_for_section")
-#     group = models.ForeignKey(Group, on_delete=models.CASCADE)   # Is in (Group <-> Section) ; many to one
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
@@ -67,16 +63,17 @@ class Message(models.Model):
     subject = models.CharField(max_length=255)
     text = models.TextField()
 
+
 class Bid(models.Model):
     team_members = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     is_approved = models.BooleanField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE) # For (Bid <-> Project) ; many to one
-    #students = models.ManyToManyField(User, related_name="students")
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     instructors = models.ManyToManyField(User, related_name="instructors_for_bid")
+
 
 class Notification(models.Model):
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_recipient")
     subject = models.CharField(max_length=255)
     text = models.TextField()
-
