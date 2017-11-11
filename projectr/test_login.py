25dc3c92f71255e37334d4c89b474313db75c001
maxtrussell <maxtrussell@gatech.edu>
2017-11-11 15:33:44 -0500

    Added login_test to testing suite

---
+++ projectr/test_login.py
@@ -0,0 +1,48 @@
+from projectr import views
+from django.test import TestCase
+from models import Project, Message, Bid, Section, Notification, Question, InstructorKey, Tag
+import mock
+
+class TestLogin(TestCase):
+
+	def test_login_student(self):
+
+		# Create a mock user
+		mockUser = mock.Mock()
+		mockUser.is_authenticated.return_value = True
+		mockUser.profile.user_type = 'S'
+
+		# Create a mock login request
+		mockReq = mock.Mock()
+		mockReq.method = "POST"
+		mockReq.user = mockUser
+
+		assert "student" in str(views.login_view(mockReq)['Location'])
+
+	def test_login_teacher(self):
+
+		# Create a mock user
+		mockUser = mock.Mock()
+		mockUser.is_authenticated.return_value = True
+		mockUser.profile.user_type = 'I'
+
+		# Create a mock login request
+		mockReq = mock.Mock()
+		mockReq.method = "POST"
+		mockReq.user = mockUser
+
+		assert "instructor" in str(views.login_view(mockReq)['Location'])
+
+	def test_login_client(self):
+
+		# Create a mock user
+		mockUser = mock.Mock()
+		mockUser.is_authenticated.return_value = True
+		mockUser.profile.user_type = 'C'
+
+		# Create a mock login request
+		mockReq = mock.Mock()
+		mockReq.method = "POST"
+		mockReq.user = mockUser
+
+		assert "client" in str(views.login_view(mockReq)['Location'])
